c7a0962bad1949f0857791d71d86c50e9db846d2
Keri <79213053+KeriGabriel@users.noreply.github.com>
2022-02-18 12:15:15 -0800

    ticket

---
+++ CentraliaDevTools/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CentraliaDevTools.Models
+{
+    public class Ticket
+    {
+        
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Location { get; set; }
+            public string Description { get; set; }
+            [DataType(DataType.Date)]
+            public DateTime CreatedOn { get; set; }
+        
+
+    }
+}
