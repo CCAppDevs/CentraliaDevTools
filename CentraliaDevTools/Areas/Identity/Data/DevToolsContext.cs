9a2073523b5e7e138d1d33cd89e28feda4d1fa24
Jordan Martin <jordanmartin.it@hotmail.com>
2022-02-19 11:51:25 -0800

    Scaffolded a Ticket Controller added Navigation.

--- CentraliaDevTools/Areas/Identity/Data/DevToolsContext.cs
+++ CentraliaDevTools/Areas/Identity/Data/DevToolsContext.cs
@@ -24,4 +24,6 @@ public class DevToolsContext : IdentityDbContext<DevToolsUser>
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
     }
+
+    public DbSet<CentraliaDevTools.Models.Ticket> Ticket { get; set; }
 }
