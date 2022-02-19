d5527fadd35639129ec4f03a8a9fbdee5ff23a3d
PowellAndrea <80601370+PowellAndrea@users.noreply.github.com>
2022-02-19 12:09:27 -0800

    controllers/views/navigation

---
+++ CentraliaDevTools/Controllers/TeamProjectMembersController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CentraliaDevTools.Data;
+using CentraliaDevTools.Models;
+
+namespace CentraliaDevTools.Controllers
+{
+    public class TeamProjectMembersController : Controller
+    {
+        private readonly DevToolsContext _context;
+
+        public TeamProjectMembersController(DevToolsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TeamProjectMembers
+        public async Task<IActionResult> Index()
+        {
+            var devToolsContext = _context.Memberships.Include(t => t.Member).Include(t => t.TeamProject);
+            return View(await devToolsContext.ToListAsync());
+        }
+
+        // GET: TeamProjectMembers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teamProjectMember = await _context.Memberships
+                .Include(t => t.Member)
+                .Include(t => t.TeamProject)
+                .FirstOrDefaultAsync(m => m.TeamProjectMemberID == id);
+            if (teamProjectMember == null)
+            {
+                return NotFound();
+            }
+
+            return View(teamProjectMember);
+        }
+
+        // GET: TeamProjectMembers/Create
+        public IActionResult Create()
+        {
+            ViewData["MemberId"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["TeamProjectId"] = new SelectList(_context.TeamProjects, "TeamProjectID", "TeamProjectID");
+            return View();
+        }
+
+        // POST: TeamProjectMembers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TeamProjectMemberID,TeamProjectId,MemberId")] TeamProjectMember teamProjectMember)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(teamProjectMember);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MemberId"] = new SelectList(_context.Users, "Id", "Id", teamProjectMember.MemberId);
+            ViewData["TeamProjectId"] = new SelectList(_context.TeamProjects, "TeamProjectID", "TeamProjectID", teamProjectMember.TeamProjectId);
+            return View(teamProjectMember);
+        }
+
+        // GET: TeamProjectMembers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teamProjectMember = await _context.Memberships.FindAsync(id);
+            if (teamProjectMember == null)
+            {
+                return NotFound();
+            }
+            ViewData["MemberId"] = new SelectList(_context.Users, "Id", "Id", teamProjectMember.MemberId);
+            ViewData["TeamProjectId"] = new SelectList(_context.TeamProjects, "TeamProjectID", "TeamProjectID", teamProjectMember.TeamProjectId);
+            return View(teamProjectMember);
+        }
+
+        // POST: TeamProjectMembers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TeamProjectMemberID,TeamProjectId,MemberId")] TeamProjectMember teamProjectMember)
+        {
+            if (id != teamProjectMember.TeamProjectMemberID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(teamProjectMember);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeamProjectMemberExists(teamProjectMember.TeamProjectMemberID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MemberId"] = new SelectList(_context.Users, "Id", "Id", teamProjectMember.MemberId);
+            ViewData["TeamProjectId"] = new SelectList(_context.TeamProjects, "TeamProjectID", "TeamProjectID", teamProjectMember.TeamProjectId);
+            return View(teamProjectMember);
+        }
+
+        // GET: TeamProjectMembers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teamProjectMember = await _context.Memberships
+                .Include(t => t.Member)
+                .Include(t => t.TeamProject)
+                .FirstOrDefaultAsync(m => m.TeamProjectMemberID == id);
+            if (teamProjectMember == null)
+            {
+                return NotFound();
+            }
+
+            return View(teamProjectMember);
+        }
+
+        // POST: TeamProjectMembers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var teamProjectMember = await _context.Memberships.FindAsync(id);
+            _context.Memberships.Remove(teamProjectMember);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeamProjectMemberExists(int id)
+        {
+            return _context.Memberships.Any(e => e.TeamProjectMemberID == id);
+        }
+    }
+}
---
+++ CentraliaDevTools/Controllers/TicketMembersController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CentraliaDevTools.Data;
+using CentraliaDevTools.Models;
+
+namespace CentraliaDevTools.Controllers
+{
+    public class TicketMembersController : Controller
+    {
+        private readonly DevToolsContext _context;
+
+        public TicketMembersController(DevToolsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TicketMembers
+        public async Task<IActionResult> Index()
+        {
+            var devToolsContext = _context.TicketMembers.Include(t => t.Member).Include(t => t.Ticket);
+            return View(await devToolsContext.ToListAsync());
+        }
+
+        // GET: TicketMembers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketMember = await _context.TicketMembers
+                .Include(t => t.Member)
+                .Include(t => t.Ticket)
+                .FirstOrDefaultAsync(m => m.TicketMemberID == id);
+            if (ticketMember == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticketMember);
+        }
+
+        // GET: TicketMembers/Create
+        public IActionResult Create()
+        {
+            ViewData["MemberId"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["TicketId"] = new SelectList(_context.Ticket, "Id", "Id");
+            return View();
+        }
+
+        // POST: TicketMembers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TicketMemberID,TicketId,MemberId")] TicketMember ticketMember)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ticketMember);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MemberId"] = new SelectList(_context.Users, "Id", "Id", ticketMember.MemberId);
+            ViewData["TicketId"] = new SelectList(_context.Ticket, "Id", "Id", ticketMember.TicketId);
+            return View(ticketMember);
+        }
+
+        // GET: TicketMembers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketMember = await _context.TicketMembers.FindAsync(id);
+            if (ticketMember == null)
+            {
+                return NotFound();
+            }
+            ViewData["MemberId"] = new SelectList(_context.Users, "Id", "Id", ticketMember.MemberId);
+            ViewData["TicketId"] = new SelectList(_context.Ticket, "Id", "Id", ticketMember.TicketId);
+            return View(ticketMember);
+        }
+
+        // POST: TicketMembers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TicketMemberID,TicketId,MemberId")] TicketMember ticketMember)
+        {
+            if (id != ticketMember.TicketMemberID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ticketMember);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TicketMemberExists(ticketMember.TicketMemberID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MemberId"] = new SelectList(_context.Users, "Id", "Id", ticketMember.MemberId);
+            ViewData["TicketId"] = new SelectList(_context.Ticket, "Id", "Id", ticketMember.TicketId);
+            return View(ticketMember);
+        }
+
+        // GET: TicketMembers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketMember = await _context.TicketMembers
+                .Include(t => t.Member)
+                .Include(t => t.Ticket)
+                .FirstOrDefaultAsync(m => m.TicketMemberID == id);
+            if (ticketMember == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticketMember);
+        }
+
+        // POST: TicketMembers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ticketMember = await _context.TicketMembers.FindAsync(id);
+            _context.TicketMembers.Remove(ticketMember);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TicketMemberExists(int id)
+        {
+            return _context.TicketMembers.Any(e => e.TicketMemberID == id);
+        }
+    }
+}
