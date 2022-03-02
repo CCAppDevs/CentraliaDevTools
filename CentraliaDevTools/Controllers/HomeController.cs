f27a097bab2b4edfd5c7e6c202d32b9f5a01f841
mark burya <mark.burya@studentcentralia.onmicrosoft.com>
2022-03-01 23:12:58 -0800

    v2

--- CentraliaDevTools/Controllers/HomeController.cs
+++ CentraliaDevTools/Controllers/HomeController.cs
@@ -2,6 +2,10 @@
 using CentraliaDevTools.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using CentraliaDevTools.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using CentraliaDevTools.Areas.Identity.Data;
 
 namespace CentraliaDevTools.Controllers
 {
@@ -10,6 +14,19 @@ namespace CentraliaDevTools.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly ITicketService _ticketService;
 
+        private readonly DevToolsContext _context;
+        private readonly UserManager<DevToolsUser> _userManager;
+
+        public async Task<IActionResult> Dashboard()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            // Filter tickets to just those that the currently logged in user is not a part of (in the TicketMembers list)
+            var filteredContext = _context.Ticket.Include(t => t.TicketMembers).Include(t => t.TicketStatus).Where(ticket => ticket.TicketMembers.Any(m => m.MemberId != user.Id));
+
+            // Pass filtered data to the view
+            return View(await filteredContext.ToListAsync());
+        }
         public HomeController(ILogger<HomeController> logger, ITicketService ticketService)
         {
             _logger = logger;
@@ -18,8 +35,10 @@ namespace CentraliaDevTools.Controllers
 
         public IActionResult Index()
         {
+
             ViewData["RandomID"] = _ticketService.GetRandomUserID();
             ViewData["JesseID"] = _ticketService.GetUserIDByName("jesse@jesse.com");
+
             return View();
         }
 
