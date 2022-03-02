8e73435ac0024af25dff8a3eaa599ccffdc00f10
mark burya <mark.burya@studentcentralia.onmicrosoft.com>
2022-03-02 01:13:31 -0800

    dashboard created

--- CentraliaDevTools/Controllers/HomeController.cs
+++ CentraliaDevTools/Controllers/HomeController.cs
@@ -1,26 +1,57 @@
-﻿using CentraliaDevTools.Infrastructure;
+﻿using CentraliaDevTools.Areas.Identity.Data;
+using CentraliaDevTools.Data;
+using CentraliaDevTools.Infrastructure;
 using CentraliaDevTools.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CentraliaDevTools.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ITicketService _ticketService;
+        private readonly DevToolsContext _context;
+        private readonly UserManager<DevToolsUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger, ITicketService ticketService)
+        public HomeController(ILogger<HomeController> logger, ITicketService ticketService, DevToolsContext context, UserManager<DevToolsUser> userManager)
         {
             _logger = logger;
             _ticketService = ticketService;
+            _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             ViewData["RandomID"] = _ticketService.GetRandomUserID();
             ViewData["JesseID"] = _ticketService.GetUserIDByName("jesse@jesse.com");
-            return View();
+
+            // Get current user
+            var user = await _userManager.GetUserAsync(User);
+
+            // Filter tickets to just those that the currently logged in user is not a part of (in the TicketMembers list)
+            var filteredContext = _context.Ticket.Include(t => t.TicketMembers).Where(ticket => ticket.TicketMembers.Any(m => m.MemberId != user.Id));
+
+            var newviewModel = new TicketIndexViewModel
+            {
+                ClosedTickets = _context.Ticket
+                   .Include(t => t.TicketMembers)
+                   .Include(t => t.TicketStatus)
+                   .Where(ticket => ticket.TicketMembers.Any(m => m.MemberId == user.Id) && ticket.TicketStatusId == 2).ToList(),
+
+                OpenTickets = _context.Ticket
+                   .Include(t => t.TicketMembers)
+                   .Include(t => t.TicketStatus)
+                   .Where(ticket => ticket.TicketMembers.Any(m => m.MemberId == user.Id) && ticket.TicketStatusId == 1).ToList(),
+            };
+
+
+            return View(newviewModel);
         }
 
         public IActionResult Privacy()
